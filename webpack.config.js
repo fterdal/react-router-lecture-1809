0bc66a131eed82af91be4d19e4ead81d2106095f
Finn Terdal <fterdal@gmail.com>
2018-05-15 16:59:25 -0500

    Added some (untested) skeleton

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: 'development',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './react/index.js',
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
+
