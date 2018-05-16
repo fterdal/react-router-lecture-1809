1c8c4c606c037334c4138c168b6539748a92253b
Finn Terdal <fterdal@gmail.com>
2018-05-16 11:57:16 -0500

    Friendlier App component

--- react/index.js
+++ react/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
-console.log("Hello, I'm React (with Babel)!")
+
 const App = () => (
-  <h1>Hello from React!</h1>
+  <div>
+    <h1>Hello from React!</h1>
+    <p>Riddle: We come at night without being fetched; we disappear by day without being stolen. What are we?</p>
+    <p style={{fontStyle: 'italic'}}>Pop open the developer tools to see the answer.</p>
+  </div>
 )
+
+console.log(`Answer:
+✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ 
+✨ ✨ ✨ Stars ✨ ✨ ✨
+✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ 
+`)
 
 render(
   <App />,
